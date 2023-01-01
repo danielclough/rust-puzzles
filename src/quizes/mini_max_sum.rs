9103d0bea6177a711db6aaa328683cf9b5d68f79
danielclough <danielclough@users.noreply.github.com>
2023-01-01 05:03:48 -0800

    finish problem 2

--- src/quizes/mini_max_sum.rs
+++ src/quizes/mini_max_sum.rs
@@ -2,16 +2,28 @@ pub fn test(arr: &str) {
     let split: Vec<&str> = arr.split("\n").collect();
     
     let str_arr: Vec<&str> = split[0].split(' ').collect();
-    let mut i32_arr: Vec<i32> = vec![];
+    let mut i64_arr: Vec<i64> = vec![];
     for ele in str_arr {
-        i32_arr.push(ele.parse::<i32>().expect("number here"));
+        i64_arr.push(ele.parse::<i64>().expect("number here"));
     };
-    miniMaxSum(&i32_arr);
+    miniMaxSum(&i64_arr);
 }
 
 #[allow(non_snake_case)]
-fn miniMaxSum(arr: &[i32]) {
-    for ele in arr {
-        println!("{ele}")
+fn miniMaxSum(arr: &[i64]) {
+    let mut sort: Vec<i64> = arr.to_owned();
+    sort.sort(); 
+    let mut min = 0;
+    let mut max = 0;
+    for (i, ele) in &mut sort.iter().enumerate() {
+        if i == 0 {
+            min += ele;
+        } else if i == sort.len()-1  {
+            max += ele;
+        } else {
+            min += ele;
+            max += ele;
+        };
     }
+    println!{"{} {}", min, max};
 }
