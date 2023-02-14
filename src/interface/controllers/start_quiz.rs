45693ce301071d81c30628bbfed647db90190a5a
danielclough <danielclough@users.noreply.github.com>
2023-02-13 23:09:21 -0800

    refactor

--- src/interface/controllers/start_quiz.rs
+++ src/interface/controllers/start_quiz.rs
@@ -1,15 +1,77 @@
-use crate::interface::types::{MenuConfig, MenuItem};
+use tui::widgets::ListState;
+
+use crate::interface::types::{MenuConfig, MenuItem, Comparison, QuizList};
 use std::{
     fs,
     io::Error,
-    process::{Command, Output, Stdio},
+    process::{Command, Output, Stdio}, time::{SystemTime, UNIX_EPOCH},
 };
+
+use super::read::read_quiz_list;
 
 pub fn exec(menu_config: &MenuConfig) {
     match menu_config.active_item {
         MenuItem::Home => {}
         _ => {}
     }
+}
+
+
+pub fn get_elapsed() -> String {
+        // create timestamp and store it
+    let duration_since_epoch = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let timestamp: String;
+    let timestamp_path = "./user-data/.timestamp";
+    let read_timestamp = std::fs::read_to_string(timestamp_path);
+    if read_timestamp.is_ok() {
+        timestamp = read_timestamp.unwrap()
+    } else {
+        timestamp = format!("{}",duration_since_epoch.as_secs());
+        _ = std::fs::write(timestamp_path, &timestamp);
+    }
+
+    let elapsed_in_sec = duration_since_epoch.as_secs() as i64 - timestamp.parse::<i64>().unwrap();
+    let elapsed_hrs = elapsed_in_sec / (60*60);
+    let elapsed_mins = elapsed_in_sec % (60*60) / 60;
+    let elapsed_secs = elapsed_in_sec % 60;
+    let elapsed = format!("{}:{}:{}", elapsed_hrs, elapsed_mins, elapsed_secs);
+    elapsed
+}
+
+pub fn init_compare(selected_quiz: &QuizList) -> Comparison {
+    //     // create random number for attempt
+    // let mut rand_thread = rand::thread_rng();
+    // let rand_n: [i32;6] = rand_thread.gen();
+
+    let elapsed = get_elapsed();
+
+    let input_path = &format!(
+        "src/quizzes/level{}/{}.txt",
+        selected_quiz.level, selected_quiz.name
+    );
+    let comparison_tupl = compare_results(input_path);
+    let input_str = &comparison_tupl.0;
+    let correct_vec: &Vec<String> = &comparison_tupl
+        .1
+        .split("\n")
+        .map(|x| x.to_string())
+        .collect();
+    let user_vec: &Vec<String> = &comparison_tupl
+        .2
+        .split("\n")
+        .map(|x| x.to_string())
+        .collect();
+    let correct_str = correct_vec.join("; ");
+    let user_str = user_vec.join("; ");
+
+    let comparison = Comparison {
+        input_str: input_str.to_string(),
+        correct_str,
+        user_str,
+        is_correct: comparison_tupl.3,
+        elapsed,
+    };
+    comparison
 }
 
 pub fn test_correct_quiz(file: &str) -> Result<Output, Error> {
@@ -75,4 +137,17 @@ fn main() {
         fs::write(new_quiz_path, new_quiz_content)?;
     };
     Ok(new_quiz_path.to_string())
+}
+
+
+pub fn get_selected_quiz(quiz_list_state: &ListState) -> QuizList {
+    let quiz_list: Vec<QuizList> = read_quiz_list().expect("can fetch quiz list");
+    quiz_list
+        .get(
+            quiz_list_state
+                .selected()
+                .expect("there is always a selected quiz"),
+        )
+        .expect("exists")
+        .clone()
 }
--- src/interface/render/start.rs
+++ src/interface/render/start.rs
@@ -1,5 +1,3 @@
-use std::time::{SystemTime, UNIX_EPOCH};
-
 // use rand::Rng;
 use tui::{
     layout::Alignment,
@@ -9,79 +7,14 @@ use tui::{
 };
 
 use crate::interface::{
-    controllers::{read::read_quiz_list, start_quiz::compare_results},
-    types::{Comparison, QuizList},
+    controllers::{
+        start_quiz::{init_compare, get_selected_quiz},
+    },
 };
-
-pub fn get_elapsed() -> String {
-        // create timestamp and store it
-    let duration_since_epoch = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
-    let timestamp: String;
-    let timestamp_path = "./user-data/.timestamp";
-    let read_timestamp = std::fs::read_to_string(timestamp_path);
-    if read_timestamp.is_ok() {
-        timestamp = read_timestamp.unwrap()
-    } else {
-        timestamp = format!("{}",duration_since_epoch.as_secs());
-        _ = std::fs::write(timestamp_path, &timestamp);
-    }
-
-    let elapsed_in_sec = duration_since_epoch.as_secs() as i64 - timestamp.parse::<i64>().unwrap();
-    let elapsed_hrs = elapsed_in_sec / (60*60);
-    let elapsed_mins = elapsed_in_sec % (60*60) / 60;
-    let elapsed_secs = elapsed_in_sec % 60;
-    let elapsed = format!("{}:{}:{}", elapsed_hrs, elapsed_mins, elapsed_secs);
-    elapsed
-}
-
-pub fn init_compare(selected_quiz: &QuizList) -> Comparison {
-    //     // create random number for attempt
-    // let mut rand_thread = rand::thread_rng();
-    // let rand_n: [i32;6] = rand_thread.gen();
-
-    let elapsed = get_elapsed();
-
-    let input_path = &format!(
-        "src/quizzes/level{}/{}.txt",
-        selected_quiz.level, selected_quiz.name
-    );
-    let comparison_tupl = compare_results(input_path);
-    let input_str = &comparison_tupl.0;
-    let correct_vec: &Vec<String> = &comparison_tupl
-        .1
-        .split("\n")
-        .map(|x| x.to_string())
-        .collect();
-    let user_vec: &Vec<String> = &comparison_tupl
-        .2
-        .split("\n")
-        .map(|x| x.to_string())
-        .collect();
-    let correct_str = correct_vec.join("; ");
-    let user_str = user_vec.join("; ");
-
-    let comparison = Comparison {
-        input_str: input_str.to_string(),
-        correct_str,
-        user_str,
-        is_correct: comparison_tupl.3,
-        elapsed,
-    };
-    comparison
-}
 
 pub fn render<'a>(quiz_list_state: &ListState) -> Paragraph<'a> {
     // init state
-    let quiz_list: Vec<QuizList> = read_quiz_list().expect("can fetch quiz list");
-    let selected_quiz = quiz_list
-        .get(
-            quiz_list_state
-                .selected()
-                .expect("there is always a selected quiz"),
-        )
-        .expect("exists")
-        .clone();
-
+    let selected_quiz = get_selected_quiz(quiz_list_state);
     let comparison = init_compare(&selected_quiz);
 
     // Render Container
